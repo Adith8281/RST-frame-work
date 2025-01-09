0c66981b94e12c52b23d62da7b6ac94b09b87c18
adith <adithkkadith@gmail.com>
2025-01-09 17:28:27 +0530

    API-class

--- project/app/views.py
+++ project/app/views.py
@@ -7,6 +7,9 @@ from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework import generics,mixins
+
 
 # Create your views here.
 
@@ -72,22 +75,64 @@ from rest_framework import status
 #             return JsonResponse(s.errors,status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET','PUT','DELETE'])
-def fun1(req,pk):
-    try:
-        demo=student.objects.get(pk=pk)
-    except student.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    if req.method=='GET':
-        s=studmodelSerializer(demo)
-        return Response(s.data)
-    elif req.method=='PUT':
-        s=studmodelSerializer(demo,data=req.data)
-        if s.is_valid():
-            s.save()
+# @api_view(['GET','PUT','DELETE'])
+# def fun1(req,pk):
+#     try:
+#         demo=student.objects.get(pk=pk)
+#     except student.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+#     if req.method=='GET':
+#         s=studmodelSerializer(demo)
+#         return Response(s.data)
+#     elif req.method=='PUT':
+#         s=studmodelSerializer(demo,data=req.data)
+#         if s.is_valid():
+#             s.save()
+#             return Response(s.data)
+#         else:
+#             return Response(status=status.HTTP_400_BAD_REQUEST)
+#     elif req.method=='DELETE':
+#         demo.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)       
+
+
+# class fun1(APIView):
+#     def get(self,request):
+#         demo=student.objects.all()
+#         s=studmodelSerializer(demo,many=True)
+#         return Response(s.data)
+#     def post(self,request):
+#         s=studmodelSerializer(data=request.data)
+#         if s.is_valid():
+#             s.save()
+#             return JsonResponse(s.data,status=status.HTTP_201_CREATED) 
+#         else:
+#             return JsonResponse(status=status.HTTP_400_BAD_REQUEST)
+
+
+class fun1(APIView):
+    def get(self,req,pk):
+        try:
+            demo=student.objects.get(pk=pk)
+            s=studmodelSerializer(demo)
             return Response(s.data)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-    elif req.method=='DELETE':
-        demo.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)        
+        except student.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    def put(self,req,pk):
+        try:
+            demo=student.objects.get(pk=pk)
+            s=studmodelSerializer(demo,data=req.data)
+            if s.is_valid():
+                s.save()
+                return Response(s.data)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        except student.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    def delete(self,req,pk):
+        try:
+            demo=student.objects.get(pk=pk)
+            demo.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
